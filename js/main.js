3ad3f4ee2022f152577c2b92b35aaafc4c23bf5f
sitrasttra <sitra19991977@gmail.com>
2024-11-09 23:06:39 +0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,28 @@
+"use strict";
+let lastScrollTop = 0;
+const header = document.querySelector("header");
+window.addEventListener("scroll", function () {
+    let scrollTop = window.scrollY || document.documentElement.scrollTop;
+    if (scrollTop > lastScrollTop) {
+        header.classList.add('rolled');
+    }
+    else {
+        header.classList.remove('rolled');
+    }
+    lastScrollTop = scrollTop;
+});
+const elements = document.querySelectorAll('.hidden');
+const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('visible');
+            setTimeout(() => {
+                entry.target.classList.remove('hidden');
+            }, 1500);
+            observer.unobserve(entry.target);
+        }
+    });
+}, { threshold: 0.5 });
+elements.forEach(element => {
+    observer.observe(element);
+});
